755fa3cdb6f55226e1d04d1b6548a5149dbb9624
francky13 <rabemiakatrafrancky@gmail.com>
2021-05-03 13:44:57 +0300

    z

--- src/app/service/suivi.service.ts
+++ src/app/service/suivi.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {url} from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,16 @@ export class SuiviService {
   constructor(private http: HttpClient) { }
   getstatistique(){
 
-    return this.http.get('https://wsfullstack.herokuapp.com/api/suivi/statistique');
+    return this.http.get(`${url}/api/suivi/statistique`);
   }
   // tslint:disable-next-line:typedef
   getstatistiquepays(){
-    return this.http.get('https://wsfullstack.herokuapp.com/api/suivi/statistiquepays');
+    return this.http.get(`${url}/api/suivi/statistiquepays`);
   }
   getstatistiquejournalier(){
-    return this.http.get('https://wsfullstack.herokuapp.com/api/suivi/statistiqueJour');
+    return this.http.get(`${url}/api/suivi/statistiqueJour`);
   }
     getpotiviter(){
-    return this.http.get('https://wsfullstack.herokuapp.com/api/suivi/tauxpositiviter');
+    return this.http.get(`${url}api/suivi/tauxpositiviter`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false
 };
+export const url = 'https://fullstackweb2.herokuapp.com/';
 
 /*
  * For easier debugging in development mode, you can import the following file
